a5919f787506c9f0fc958ab6409321ba1abb5cb9
Ian <iansbrizza@gmail.com>
2021-06-14 09:14:29 -0300

    Added semicolon on line 12

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express'),
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 let tmRoutes = teamRoutes(app);
