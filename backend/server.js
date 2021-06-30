dff38ca58a6832edebbf0f988e6210d64e6f36de
NicoPuccio <nico.puccio@hotmail.com>
2021-06-30 16:50:28 -0300

    Added CORS support.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 let wkQueueRoutes = workQueueRoutes(app);
 
