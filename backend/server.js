03c0d7b761b004795470c5c517af5b9f19bc73f4
RichoM <richi.moran@gmail.com>
2021-07-14 12:45:23 -0300

    Removed CORS headers. No longer necessary now that the frontend is being hosted by the server

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("../frontend/src"));
 
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
 let wkQueueRoutes = workQueueRoutes(app);
 let sticPageRoutes = staticPageRoutes(app);
 
