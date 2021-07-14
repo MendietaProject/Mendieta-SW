c0ef1141d94163e060b5877d60ad3d68907643b3
RichoM <richi.moran@gmail.com>
2021-07-14 12:59:43 -0300

    Fixed inconsistent formatting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,49 +6,48 @@ function buildButtons(){
     return button;
 }
 
-$(document).ready( function () {
-    var table = $('#table_id').DataTable({
-        "ajax": {url: "/work-queue",
-         dataSrc:''
+$(document).ready(function () {
+  const table = $('#table_id').DataTable({
+    ajax: {
+      url: "/work-queue",
+      dataSrc:''
     },
-        /*rowReorder:{
-            dataSrc: "name"
-        },*/ //TODO: I need to store the original index and display it in the datatable
-        "columns": [
-            {   "data"  :   "id" },
-            {   "data"  :   "name"  },
-            {   "data"  :   "members"  },
-            {   "data"  :   "size"  },
-            {   "defaultContent":   "<button class=\"delete\">Borrar</button>"}
-        ],
-        responsive: true,
-        "createdRow":function(row, data, index)
-        {
-            var name = data.id.toString();
-            $(row).addClass(name)
-            console.log(row)
-        },
-    });
-    table.on( 'row-reorder', function ( e, details, edit ) {
-        console.log( details );
-    } );
+    //TODO: I need to store the original index and display it in the datatable
+    columns: [
+      {"data" : "id"},
+      {"data" : "name"},
+      {"data" : "members"},
+      {"data" : "size"},
+      {"defaultContent": "<button class=\"delete\">Borrar</button>"}
+    ],
+    responsive: true,
+    createdRow: (row, data, index) => {
+      var name = data.id.toString();
+      $(row).addClass(name)
+      console.log(row)
+    },
+  });
 
-    table.on( 'click', 'button', function () {
-        var action = this.className;
-        var data = table.row( $(this).parents('tr') ).data();
-        console.log(action);
-        console.log(data);
+  table.on('row-reorder', function (e, details, edit) {
+    console.log(details);
+  });
 
-        if(action === 'delete'){ //get data, modify table
-            console.log("Numero de ID en la clase: " + data);
-            $.ajax({
-                url: "/work-queue/" + data.id,
-                type: "DELETE",
-                success: function(result){
-                    console.log("Succesfully Deleted " + result);
-                    table.ajax.reload();
-                }
-            })
+  table.on('click', 'button', function () {
+    var action = this.className;
+    var data = table.row($(this).parents('tr')).data();
+    console.log(action);
+    console.log(data);
+
+    if(action === 'delete'){ //get data, modify table
+      console.log("Numero de ID en la clase: " + data);
+      $.ajax({
+        url: "/work-queue/" + data.id,
+        type: "DELETE",
+        success: function(result){
+          console.log("Succesfully Deleted " + result);
+          table.ajax.reload();
         }
-    } );
-} );
+      })
+    }
+  });
+});
