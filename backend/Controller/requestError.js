ca6a00a4636df4e525841cc59648749dc291b740
Ian <iansbrizza@gmail.com>
2021-07-23 19:38:06 -0300

    Renamed to storageError

--- backend/Controller/requestError.js
+++
@@ -1,8 +0,0 @@
-class RequestError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.name = "RequestError";
-        this.statusCode = statusCode;
-    }
-}
-module.exports = RequestError;
