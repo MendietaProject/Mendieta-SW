13d5c0ee42c922e071b24b096a22b86d82bba060
Ian <iansbrizza@gmail.com>
2021-08-29 14:06:47 -0300

    Add more routes to ActivityController

--- backend/Controller/ActivityController.js
+++ backend/Controller/ActivityController.js
@@ -1,25 +1,78 @@
+const ActivityPointerStorage = require('../storage/activityPointerStorage');
+const StorageError = require('../storage/storageError');
+const ActivityError = require('./activityError');
+const ActivityStorage = require('../storage/activityStorage');
 
-function initActivityController(app, state) {
+function activityController(app, state) {
+  const actPtrStorage = new ActivityPointerStorage('activities');
 
-  app.route("/activities")
-    .get((req, res) => {
-      // TODO(Richo): Devolver todas las actividades para que el docente elija
-    })
+  app.route('/activities')
+    .get(getAll)
+    .put(put)
 
-  app.route("/activities/current")
-    .get((req, res) => {
-      // TODO(Richo): Si no hay actividad qué devolvemos?
-      res.send(state.currentActivity);
-    })
-    .post((req, res) => {
-      let activity = {
-        name: req.body.name,
-        students: [],
-        submissions: []
-      };
-      state.currentActivity = activity;
-      res.send(state.currentActivity)
-    });
+  app.route('/activities/:activityId')
+    .get(getById)
+
+  app.route('/activities/current')
+    .get(getCurrent)
+    .post(postCurrent);
+
+  function getAll(req, res) {
+    try {
+      res.send(actPtrStorage.getAll());
+    } catch (err) {
+      handleError(res, err);
+    }
+  }
+
+  function getById(req, res) {
+    try {
+      let result = actPtrStorage.getById(req.params.activityId);
+      let actStorage = new ActivityStorage(result.id);
+      res.send(actStorage.getAll());
+    } catch (err) {
+      handleError(res, err);
+    }
+  }
+
+  function put(req, res) {
+    try {
+      req.body["id"] = actPtrStorage.add()
+      let actStorage = new ActivityStorage(req.body.id);
+      res.send(actStorage.add(req.body));
+
+    } catch (err) {
+      handleError(res, err);
+    }
+  }
+
+  function getCurrent(req, res) {
+    try {
+      if (state.currentActivity === undefined) {
+        throw new ActivityError("There is no current activity", 404);
+      }
+      return state.currentActivity;
+    } catch (err) {
+      handleError(res, err);
+    }
+  }
+
+  function postCurrent(req, res) {
+    let activity = {
+      name: req.body.name,
+      students: [],
+      submissions: []
+    };
+    state.currentActivity = activity;
+    res.send(state.currentActivity)
+  }
+
+  function handleError(res, err) {
+    res.status(500);
+    if (err instanceof StorageError || err instanceof ActivityError)
+      res.status(err.statusCode);
+    res.send(JSON.stringify(`${err.name}: ${err.message}`));
+  }
 }
 
-module.exports = initActivityController;
+module.exports = activityController;
