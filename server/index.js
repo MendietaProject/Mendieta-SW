a353b3d2f693a8c8fa019f14de99fc8b5f1ac9b0
RichoM <richi.moran@gmail.com>
2021-10-08 10:01:34 -0300

    Changed default server port to 5000 to avoid collision with physicalbits

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const services = require("./backend/services.js");
 const queue_mgr = require("./backend/queue_mgr.js");
 
 const mendieta = new Mendieta();
-services.start(mendieta, process.env.PORT || 3000);
+services.start(mendieta, process.env.PORT || 5000);
 queue_mgr.start(mendieta, process.argv[2] || "/dev/ttyACM0");
