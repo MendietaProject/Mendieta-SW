aa1a7bdde47a9289ec8ebbadaef24e7844d51cbb
navegador <navegador@CISLAB1-80589.adm.vaneduc.edu.ar>
2023-09-07 18:23:05 -0300

    a

---
+++ server/backend/storage.js
@@ -0,0 +1,89 @@
+const sqlite3 = require('sqlite3').verbose();
+
+class Storage {
+    constructor() {
+        this.activities = [];
+        this.students = [];
+
+        this.db = new sqlite3.Database('x.db', (err) => {
+            if (err) {
+                console.error('Error', err.message);
+            } else {
+                console.log('Conectado');
+
+                this.data_students();
+                this.data_activities();
+            }
+        });
+    }
+
+    data_students() {
+        this.db.all('SELECT * FROM students', [], (err, studentRows) => {
+            if (err) {
+                console.error('Error en estudiantes:', err.message);
+                return;
+            }
+            this.students = studentRows;
+            console.log('Datos de estudiantes obtenidos.');
+
+            console.log('Datos de estudiantes:', this.students);
+        });
+    }
+
+    data_activities() {
+        this.db.all('SELECT * FROM activities', [], (err, activityRows) => {
+            if (err) {
+                console.error('Error en actividades:', err.message);
+                return;
+            }
+            this.activities = activityRows;
+            console.log('Datos de actividades obtenidos.');
+
+            console.log('Datos de actividades:', this.activities);
+        });
+    }
+
+   new_student() {
+        const stdin = process.stdin;
+        const stdout = process.stdout;
+
+        stdin.resume();
+        stdout.write('Ingrese el ID del nuevo estudiante: ');
+
+        stdin.once('data', (id) => {
+            id = id.toString().trim();
+
+            stdout.write('Ingrese el Nombre del nuevo estudiante: ');
+
+            stdin.once('data', (name) => {
+               name = name.toString().trim();
+
+                this.db.run('INSERT INTO students (id, name) VALUES (?, ?)', [id, name], (err) => {
+                    if (err) {
+                        console.error('Error al ingresar nuevo estudiante', err.message);
+                        return;
+                    }
+                    console.log("Nuevo estudiante guardado");
+
+                    this.closeDatabase();
+                    stdin.pause();
+                });
+            });
+        });
+    }
+
+    closeDatabase() {
+        this.db.close((err) => {
+            if (err) {
+                console.error('Error', err.message);
+            } else {
+                console.log('Conexión a la base de datos cerrada.');
+            }
+        });
+    }
+}
+
+const storage = new Storage();
+
+storage.new_student();
+
