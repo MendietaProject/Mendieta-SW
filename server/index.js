f38253b7c92eb1a93091a9961aba9aeea96627c4
RichoM <richi.moran@gmail.com>
2023-09-28 09:53:02 -0300

    Changed default storage.
    Until SqlStorage is fully functional we'll keep FakeStorage as default.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const services = require("./backend/services.js");
 const queue_mgr = require("./backend/queue_mgr.js");
 const { FakeStorage, SqlStorage } = require("./backend/storage.js");
 
-const storage = new SqlStorage();
+const storage = new FakeStorage();
 const mendieta = new Mendieta(storage);
 services.start(mendieta, process.env.PORT || 5000);
 queue_mgr.start(mendieta, process.argv[2] || "/dev/ttyACM0");
