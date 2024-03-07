2c59493131add224b56a994c854296de18306344
RichoM <richi.moran@gmail.com>
2024-03-07 14:25:25 -0300

    Revert "Updated turn notifier to work inside the "controls" panel"
    This reverts commit 3e18edf150345c72e7fdf8707e0e7ddb99ecee9a.

--- server/frontend/ide/turn.js
+++ server/frontend/ide/turn.js
@@ -1,6 +1,7 @@
 let TurnNotifier = (function () {
 
   let activeSubmission = null;
+  let hidingModal = false; // TODO(Richo): This sucks!
   let countdown = null;
 
   function updateGUI() {
@@ -9,7 +10,13 @@ let TurnNotifier = (function () {
 
     $("#turn-notifier-start-button").prop("disabled", submission.state == "RUNNING");
     $("#turn-notifier-pause-button").prop("disabled", submission.state != "RUNNING");
-    $("#turn-notifier-stop-button").show();
+    if (submission.state == "READY") {
+      $("#turn-notifier-stop-button").hide();
+      $("#turn-notifier-cancel-button").show();
+    } else {
+      $("#turn-notifier-stop-button").show();
+      $("#turn-notifier-cancel-button").hide();
+    }
   }
 
   function isActive(submission) {
@@ -20,23 +27,36 @@ let TurnNotifier = (function () {
     return submission.state == "COMPLETED" || submission.state == "CANCELED";
   }
 
-  function show() {
-    $("#turn-notifier").show();
-    $("#submit-button").hide();
+  function showModal() {
+    if (hidingModal) {
+      $('#turn-notifier-modal').one('hidden.bs.modal', function (e) {
+        hidingModal = false;
+        $("#turn-notifier-modal").modal("show");
+      });
+    } else {
+      $("#turn-notifier-modal").modal("show");
+    }
   }
 
-  function hide() {
-    $("#turn-notifier").hide();
-    $("#submit-button").show();
+  function hideModal() {
+    hidingModal = true;
+    $("#turn-notifier-modal").modal("hide");
   }
 
   function init() {
-    hide();
     countdown = Countdown.on($("#turn-notifier-timer"));
 
     $("#turn-notifier-start-button").on("click", () => Mendieta.start(activeSubmission));
     $("#turn-notifier-pause-button").on("click", () => Mendieta.pause(activeSubmission));
     $("#turn-notifier-stop-button").on("click", () => Mendieta.stop(activeSubmission));
+    $("#turn-notifier-cancel-button").on("click", () => Mendieta.stop(activeSubmission));
+
+
+    // TODO(Richo): THIS SUCKS! I need to find a more elegant way of working around the async modal transitions.
+    // Or maybe just not use a modal...
+    $('#turn-notifier-modal').on('hidden.bs.modal', function (e) {
+      hidingModal = false;
+    });
 
     Mendieta.on("submission-update", evt => {
       let timestamp = evt.timestamp;
@@ -46,14 +66,14 @@ let TurnNotifier = (function () {
         activeSubmission = submission;
         if (!previous) {
           countdown.start(submission.testDuration - (timestamp - submission.testBeginTime));
-          show();
+          showModal();
         }
       } else if (isFinished(submission)) {
         // TODO(Richo): If the activeSubmission is not set but we got here then it must mean one of our pending
         // submissions got canceled, should we show a message?
         if (activeSubmission && activeSubmission.id == submission.id) {
           activeSubmission = null;
-          hide();
+          hideModal();
           countdown.stop();
         }
       }
